e07094f829f47615379cbbbfebf064a00fd4f257
claralindbloms <97987325+claralindbloms@users.noreply.github.com>
2022-02-28 14:33:47 +0100

    start javascript main

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+function createAnArtist (firstname, lastname, age, etnicity, gender){
+    let artist = {
+        firstname: firstname,
+        lastname: lastname,
+        age: age,
+        etnicity: etnicity,
+        gender: gender,
+    };
+
+    return artist;
+}
+
+function addNewArtistToChart(chart, artist){
+    chart.push(artist);
+}
+
+function placeArtist (artist) {
+    let artistsElement = document.querySelector("#artists");
+    artistsElement.innerHTML = "";
+}
